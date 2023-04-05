972551b70db6477ce167205e86d4d56fde148063
Michal Javor <javor.mi.2020@skola.ssps.cz>
2023-04-06 00:25:06 +0200

    ✨ Added example of private db

---
+++ src/db/private.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE tokens (
+    token_hash TEXT NOT NULL,
+    admin BOOLEAN NOT NULL
+);
+
+
+
+INSERT INTO tokens (token_hash, admin)
+VALUES
+    ('20e188ee80770a47aa06085cefb37b2fc24302426b76e8a7c0cbb83162b47f8e','t'), ('e42da6130b708a25b1502a0b5ffb0ed23aa9bf62c675f62ed331c241b82dd015', 'f');
