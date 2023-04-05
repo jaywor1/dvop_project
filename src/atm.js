09b9910ddc85039dce1959f4d3b67380f0c0db09
Michal Javor <javor.mi.2020@skola.ssps.cz>
2023-04-06 01:00:41 +0200

    ✨ Added more routes

--- src/atm.js
+++ src/atm.js
@@ -1,22 +1,81 @@
 const express = require('express')
+const pg = require('pg')
 const router = express.Router()
 
-router.get('/atm', (req, res) => {
-    // list of all atms
+const pool = new pg.Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "db",
+    password: "postgres",
+    port: 5432
 })
 
-router.get('/atm/broken', (req, res) => {
-    // returns broken atms
+router.get('/atm', async (req, res) => {
+    console.log("GET /atm")
+    const client = await pool.connect();
+    try {
+        const result = await client.query('SELECT * FROM atms')
+        res.send(result.rows)
+    } finally {
+        client.release();
+    }
 })
 
-router.get('/atm/refil', (req, res) => {
-    // returns atms that need refil
+router.get('/atm/:atm_id', async (req, res) => {
+    console.log("GET /atm/" + req.params.atm_id + "/log")
+    const client = await pool.connect();
+
+    client.query('SELECT * FROM atms WHERE atm_id = $1', [req.params.atm_id], (err, result) => {
+        if (err) {
+            console.log(err.stack)
+            client.release();
+        }
+        else {
+            res.status(200).json(result.rows)
+            client.release();
+        }
+    })
+
 })
 
-router.get('/atm/:atm_id/log', (req, res) => {
-    // returns withdraw_log
+router.get('/atm/broken', async (req, res) => {
+    console.log("GET /atm/broken")
+    const client = await pool.connect();
+    try {
+        const result = await client.query('SELECT atm_id,error_log FROM atms WHERE error_log is not null')
+        res.json(result.rows)
+    } finally {
+        client.release();
+    }
 })
 
+router.get('/atm/refil', async (req, res) => {
+    console.log("GET /atm/refil")
+    const client = await pool.connect();
+    try {
+        const result = await client.query('SELECT * FROM atms WHERE stock < 20000')
+        res.send(result.rows)
+    } finally {
+        client.release();
+    }
+})
+
+router.get('/atm/:atm_id/log', async (req, res) => {
+    console.log("GET /atm/" + req.params.atm_id + "/log")
+    const client = await pool.connect();
+
+    client.query('SELECT withdraw_log FROM atms WHERE atm_id = $1', [req.params.atm_id], (err, result) => {
+        if (err) {
+            console.log(err.stack)
+            client.release();
+        }
+        else {
+            res.status(200).json(result.rows)
+            client.release();
+        }
+    })
+
+})
 router.get('/atm/:atm_id/error', (req, res) => {
     // returns error log
 })
