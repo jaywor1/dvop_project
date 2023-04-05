87123d8b24cbf037bffe9404c32c5e305d3fb5e8
Michal Javor <javor.mi.2020@skola.ssps.cz>
2023-04-06 00:14:27 +0200

    ✨ Added atm routes to app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const express = require('express')
-
+const pg = require('pg')
+const atm_route = require('./atm')
 
 const app = express();
+
+
+const pool = new pg.Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "db",
+    password: "postgres",
+    port: 5432
+})
+
+
+app.use('/', atm_route)
+
+
 
 
 app.listen(3000, () => {
