613f7735ea59b4276248a20839ba44b92ef0ff03
Michal Javor <javor.mi.2020@skola.ssps.cz>
2023-04-06 02:58:32 +0200

    ✨ Added put of employe

--- src/routes/employe.js
+++ src/routes/employe.js
@@ -28,5 +28,34 @@ router.get('/employe', checkAdmin, async (req, res) => {
 
 })
 
+router.put('/employe', checkAdmin, express.json(), async (req, res) => {
+    console.log("PUT /employe")
+    const client = await public.connect();
+
+    reqBody = req.body;
+
+    params = [reqBody.branch_id, reqBody.name, reqBody.position, reqBody.present]
+
+    console.log(params)
+
+    for (par of params) {
+        if (par == undefined)
+            return res.status(400).send("Invalid request")
+    }
+
+    client.query('INSERT INTO employes (branch_id, name, position, present) VALUES ($1, $2, $3, $4)', params, (err, result) => {
+        if (err) {
+            console.log(err.stack)
+            client.release();
+        }
+        else {
+            res.status(200).send("Success")
+            client.release();
+        }
+    })
+
+})
+
+
 
 module.exports = router;
