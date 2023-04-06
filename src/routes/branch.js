aef54d842f2b40f4a7372005fb4b85eee3280169
Michal Javor <javor.mi.2020@skola.ssps.cz>
2023-04-06 02:11:17 +0200

    🐛 Fixed bugs

--- src/routes/branch.js
+++ src/routes/branch.js
@@ -27,8 +27,28 @@ router.get('/branch', checkAdmin, async (req, res) => {
     })
 
 })
+router.post('/branch', checkAdmin, express.json(), async (req, res) => {
+    console.log("POST /branch")
+    const reqBody = req.body;
+    const client = await public.connect();
 
-router.post('/branch', async (req, res) => {
+    const params = [reqBody.open_hours, reqBody.close_hours, reqBody.address]
+
+    for (par of params) {
+        if (par == undefined)
+            return res.status(400).send("Invalid request")
+    }
+
+    client.query('INSERT INTO branch (open_hours, close_hours, address) VALUES ($1, $2, $3)', params, (err, result) => {
+        if (err) {
+            console.log(err.stack)
+            client.release();
+        }
+        else {
+            res.status(200).json(result.rows)
+            client.release();
+        }
+    })
 
 })
 
