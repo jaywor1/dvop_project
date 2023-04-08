b20127d850c6a0f50e57eefc9ecfce249e315fb2
jaywor1 <misa.javor@gmail.com>
2023-04-08 02:52:56 +0200

    ✨ Added ATM constructor

--- console/console_client/console_client/ATM.cs
+++ console/console_client/console_client/ATM.cs
@@ -11,5 +11,12 @@ namespace console_client
         public int atm_id { get; set; }
         public int stock {get; set; }
         public bool error { get; set; }
+
+        public ATM(int atm_id, int stock, bool error)
+        {
+            this.atm_id = atm_id;
+            this.stock = stock;
+            this.error = error;
+        }
     }
 }
