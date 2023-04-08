6cd7c1d1ba207fd708c84c0f46c5a8790bf478ec
jaywor1 <misa.javor@gmail.com>
2023-04-08 03:01:27 +0200

    ♻️ Changed ATM object

--- console/console_client/console_client/ATM.cs
+++ console/console_client/console_client/ATM.cs
@@ -8,15 +8,18 @@ namespace console_client
 {
     public class ATM
     {
-        public int atm_id { get; set; }
-        public int stock {get; set; }
-        public bool error { get; set; }
+        public int atm_id;
+        public int stock;
+        public bool error;
+        public string address;
 
-        public ATM(int atm_id, int stock, bool error)
+
+        public ATM(int atm_id, int stock, bool error, string address)
         {
             this.atm_id = atm_id;
             this.stock = stock;
             this.error = error;
+            this.address = address;
         }
     }
 }
