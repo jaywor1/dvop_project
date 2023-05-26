caa3d267ccba38ea273dcfe0f7f83830a24323d3
jaywor1 <misa.javor@gmail.com>
2023-05-26 05:12:41 +0200

    ✨ Added branch_id to ATM

--- console/console_client/console_client/ATM.cs
+++ console/console_client/console_client/ATM.cs
@@ -9,16 +9,16 @@ namespace console_client
     public class ATM
     {
         public int atm_id { get; set; }
+        public int branch_id { get; set; }
         public int stock { get; set; }
         public string address { get; set; }
-        public bool error { get; set; }
 
 
-        public ATM(int atm_id, int stock, string address, bool error)
+        public ATM(int atm_id, int branch_id, int stock, string address)
         {
             this.atm_id = atm_id;
+            this.branch_id = branch_id;
             this.stock = stock;
-            this.error = error;
             this.address = address;
         }
     }
